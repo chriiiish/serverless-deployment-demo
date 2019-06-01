a349ef36ee52d1bf858e0d7adab803c7d55d7a5a
Chris Lloyd <chris.lloyd.hi@gmail.com>
2019-06-01 21:45:17 +1200

    Deploy a very basic "Hello World" api method

--- back-end/Handler.cs
+++ back-end/Handler.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 using System;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -7,33 +8,12 @@ namespace Serverless.DeploymentDemo
 {
     public class Handler
     {
-       public Response Hello(Request request)
+       public APIGatewayProxyResponse Hello(APIGatewayProxyRequest request, ILambdaContext context)
        {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
+           return new APIGatewayProxyResponse{
+             StatusCode = 200,
+             Body = "Go Serverless v1.0! Your function executed successfully!"
+           };
        }
-    }
-
-    public class Response
-    {
-      public string Message {get; set;}
-      public Request Request {get; set;}
-
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
-    }
-
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
-
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
-      }
     }
 }
--- back-end/README.md
+++ back-end/README.md
@@ -3,3 +3,9 @@ This spins up a serverless API to serve requests from the front-end.
 
 Written in: C# (dotnetcore2.1)
 
+## Usage
+First build:
+`./build.sh`
+
+Then deploy:
+`sls deploy --aws-profile {your-profile-name}`
