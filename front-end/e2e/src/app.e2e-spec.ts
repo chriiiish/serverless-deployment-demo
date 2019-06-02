a107e8280e66f210e3db0582b98ddf2a6494f703
Chris Lloyd <chris.lloyd.hi@gmail.com>
2019-06-03 08:03:18 +1200

    Add End-to-End tests for front-end

--- front-end/e2e/src/app.e2e-spec.ts
+++ front-end/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,36 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Deployment Test Page', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have the right title', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to front-end!');
+    expect(page.getTitleText()).toEqual('Deployment Method Demonstrator');
+  });
+
+  it('should have two panels', () => {
+    page.navigateTo();
+    expect(page.getPanels().count()).toEqual(2);
+  });
+
+  it('should have an input box in each panel', () => {
+    page.navigateTo();
+    expect(page.getPanelInputBoxes().count()).toEqual(2);
+  });
+
+  it('should have a button in each panel', () => {
+    page.navigateTo();
+    expect(page.getPanelButtons().count()).toEqual(2);
+  });
+
+  it('should not have any tweets on initial load', () => {
+    page.navigateTo();
+    expect(page.getTweets().count()).toEqual(0);
   });
 
   afterEach(async () => {
--- front-end/e2e/src/app.po.ts
+++ front-end/e2e/src/app.po.ts
@@ -5,7 +5,23 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
+  getPanelButtons(){
+    return element.all(by.css('div.panel button'));
+  }
+
+  getPanelInputBoxes(){
+    return element.all(by.css('div.panel input'));
+  }
+
+  getPanels(){
+    return element.all(by.css('div.panel'));
+  }
+
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+
+  getTweets(){
+    return element.all(by.css('div.tweet'));
   }
 }
