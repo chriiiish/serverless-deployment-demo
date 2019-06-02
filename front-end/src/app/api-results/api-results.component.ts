4b48770502a3fcd6f7ca3f14dc9fd41b716e51c3
Chris Lloyd <chris.lloyd.hi@gmail.com>
2019-06-02 22:34:41 +1200

    Hooked up real responses to site

--- front-end/src/app/api-results/api-results.component.ts
+++ front-end/src/app/api-results/api-results.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiResponse } from '../ApiResponse';
 
 @Component({
   selector: 'app-api-results',
@@ -8,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class ApiResultsComponent implements OnInit {
 
-  v1tweets = [ {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
-              {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
-  v2tweets = [ {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
-              {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
+  @Input() v1tweets: Array<ApiResponse>;
+  @Input() v2tweets: Array<ApiResponse>;
+  
+  // v1tweets = [ {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
+  //             {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
+  // v2tweets = [ {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
+  //             {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
 
   constructor() { }
 
