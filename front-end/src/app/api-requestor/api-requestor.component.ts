61829c5eeb035147ec800b11504176105757ed38
Chris Lloyd <chris.lloyd.hi@gmail.com>
2019-06-03 07:40:19 +1200

    Tidy up the Angular code a bit

--- front-end/src/app/api-requestor/api-requestor.component.ts
+++ front-end/src/app/api-requestor/api-requestor.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ApiResponse } from '../ApiResponse';
-import { CONTEXT_NAME } from '@angular/compiler/src/render3/view/util';
+import { ApiResponse } from '../models/ApiResponse';
 
 @Component({
   selector: 'app-api-requestor',
@@ -12,16 +11,17 @@ export class ApiRequestorComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  interval = undefined; // The refresh timer interval
-  v1responses = [];     // V1 API Responses
-  v2responses = [];     // V2 API Responses
-  updateMethod;         // The method called to get url responses
-  startRequests = true; // Should the next button click start requests
+  interval = undefined;     // The refresh timer interval
+  v1responses = [];         // V1 API Responses
+  v2responses = [];         // V2 API Responses
+  updateMethod: Function;   // The method to call to get url responses
+  startRequests = true;     // Should the next button click start requests
 
   ngOnInit() { 
     this.updateMethod = this.Update;
   }
 
+  // Start/Stop requests to URL
   ToggleRequests(event: any, url:string){
     if (this.startRequests){
       this.StartRequests(event, url);
@@ -31,6 +31,7 @@ export class ApiRequestorComponent implements OnInit {
     this.startRequests = !this.startRequests;
   }
 
+  // Start fetching data from URL
   StartRequests(event: any, url:string)
   {
     // Start updating the records
@@ -39,19 +40,23 @@ export class ApiRequestorComponent implements OnInit {
     this.updateMethod(this, url, responses);
   }
 
+  // Stop fetching data from URL
   StopRequests(event: any){
     this.updateMethod = this.HaltUpdate;
   }
 
+  // Make some requests to the URL
+  // Once there's enough requests, save them to the responses list
   Update(context: ApiRequestorComponent, url: string, responses: ApiResponse[])
   {
     // Get 10 Requests
     if (responses.length < 10){
-      context.http.get(url).subscribe(function(context: ApiRequestorComponent, url: string, responses: ApiResponse[]){
-        return function(response: ApiResponse){
-        console.log(response);
-        responses.push(response);
-        context.updateMethod(context, url, responses);
+      context.http.get(url).subscribe(
+        function(context: ApiRequestorComponent, url: string, responses: ApiResponse[]){
+          return function(response: ApiResponse){
+            console.log(response);
+            responses.push(response);
+            context.updateMethod(context, url, responses);
       }}(context, url, responses));
     } else {
       // Process the responses
@@ -72,12 +77,17 @@ export class ApiRequestorComponent implements OnInit {
       context.v1responses = v1;
       context.v2responses = v2;
       console.log("Assigned new responses");
+
       responses = new Array<ApiResponse>();
       console.log(responses);
+      
       setTimeout(context.updateMethod, 1000, context, url, responses);
     }
   }
 
+  // This stops the update method
+  //  this.updateMethod gets set to this function to stop processing
+  //  because it has no logic
   HaltUpdate(context: ApiRequestorComponent, url: string, responses: ApiResponse[])
   { 
     console.log("stopped loop");
--- front-end/src/app/api-results/api-results.component.ts
+++ front-end/src/app/api-results/api-results.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ApiResponse } from '../ApiResponse';
+import { ApiResponse } from '../models/ApiResponse';
 
 @Component({
   selector: 'app-api-results',
@@ -11,11 +11,6 @@ export class ApiResultsComponent implements OnInit {
 
   @Input() v1tweets: Array<ApiResponse>;
   @Input() v2tweets: Array<ApiResponse>;
-  
-  // v1tweets = [ {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
-  //             {"ApiVersion":"1.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
-  // v2tweets = [ {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Canary Deployment","User":"Lambda"}},
-  //             {"ApiVersion":"2.0","Tweet":{"Message":"I'm a Linear Deployment","User":"Lambda"}} ];
 
   constructor() { }
 
